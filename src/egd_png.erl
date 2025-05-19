e76599b270aac5f9a74df2611731b74978f408f6
RaymondLoranger <Raymond.Loranger@bell.net>
2025-05-19 19:31:42 -0400

    0.1.0

--- src/egd_png.erl
+++ src/egd_png.erl
@@ -1,8 +1,8 @@
 %%
 %% %CopyrightBegin%
-%% 
+%%
 %% Copyright Ericsson AB 2008-2016. All Rights Reserved.
-%% 
+%%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -14,14 +14,14 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
-%% 
+%%
 %% %CopyrightEnd%
 
 
 %% This code was originally written by Dan Gudmundsson for png-handling in
 %% wings3d (e3d__png).
 %%
-%% @doc egd 
+%% @doc egd
 %%
 
 -module(egd_png).
@@ -95,11 +95,12 @@ filter_row(Row,_RowLen) ->
 %png_type(r8g8b8a8) -> ?TRUECOLOUR_A;
 png_type(r8g8b8) -> ?TRUECOLOUR.
 
-create_chunk(Bin,Z) when is_list(Bin) ->
-    create_chunk(list_to_binary(Bin),Z);
-create_chunk(Bin,Z) when is_binary(Bin) ->
+create_chunk(Bin,_Z) when is_list(Bin) ->
+    create_chunk(list_to_binary(Bin),_Z);
+create_chunk(Bin,_Z) when is_binary(Bin) ->
     Sz = size(Bin)-4,
-    Crc = zlib:crc32(Z,Bin),
+    %Crc = zlib:crc32(Z,Bin),
+    Crc = erlang:crc32(Bin),
     <<Sz:32,Bin/binary,Crc:32>>.
 
 % End tainted
